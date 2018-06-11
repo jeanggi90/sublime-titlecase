272da7aa7babad0bb2285c070a4775f660b542c0
Jean-Claude Graf <jeanggi90@gmail.com>
2018-06-11 23:11:47 +0200

    Implemented code for processing only text between curly brackets if no region is selected. Useful for working with LaTeX. If a region is selected or the cursor is on a line without an opening and closing curly bracket everything works as before.

--- Smart Title Case.py
+++ Smart Title Case.py
@@ -9,8 +9,23 @@ class SmartTitleCaseCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         for region in self.view.sel():
             if region.empty():
-                region = self.view.line(region)
+
+                line = self.view.line(region)
+                lineStr = self.view.substr(line)
+
+                relIndex = region.begin() - line.begin()
+
+                end = lineStr.find("}", relIndex)
+                sta = lineStr[::-1].find("{", len(lineStr) - relIndex)
+
+                if sta is not -1 and end is not -1:
+                    region = sublime.Region(line.begin() + (len(lineStr) - sta), line.begin() + end)
+
+                else:
+                    region = self.view.line(region)
 
             s = self.view.substr(region)
             s = titlecase(s)
             self.view.replace(edit, region, s)
+
+            print("tex: ", s)
